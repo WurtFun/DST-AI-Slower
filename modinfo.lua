ca811e335aa2c29322fcf084a039ad57283e0b42
WurtFun <support@wurt.fun>
2024-07-14 07:58:45 +0800

    Add files via upload

---
+++ modinfo.lua
@@ -0,0 +1,9 @@
+name = "AI Slower"
+description = "Modifies AIs on the server to make them update slow."
+author = "Wurt.fun"
+version = "rdev-0241714"
+forumthread = ""
+api_version = 10
+dst_compatible = true
+client_only_mod = false
+all_clients_require_mod = true
